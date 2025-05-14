27cc77cab7fa58095dfbd50a07b1b7d5df34c63b
diego-A-castro <daccastro05@gmail.com>
2025-05-13 22:56:57 -0500

    Added video capture feature using the laptop webcam

--- src/opencv.py
+++ src/opencv.py
@@ -1,2 +1,18 @@
-#First test comment
-#Second comment
+# import cv2 library
+import cv2
+
+# Capture the webcam. 0 is the internal camera and 1 ia for an external camera
+webcam = cv2.VideoCapture(0)
+while True:
+    ret,frame = webcam.read()
+
+    if ret == True:
+        flipped_frame = cv2.flip(frame, 1)
+        cv2.imshow("VidCapture", flipped_frame)
+        key = cv2.waitKey(1)
+        # For now click q to close the window
+        if key == ord('q'):
+            break
+
+webcam.release()
+cv2.destroyAllWindows()
