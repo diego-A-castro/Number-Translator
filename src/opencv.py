8d8d152c2daac728be77216ba5a8c45aff9278da
diego-A-castro <daccastro05@gmail.com>
2025-05-14 20:46:01 -0500

    applied hand tracking model using mediapipe

--- src/opencv.py
+++ src/opencv.py
@@ -1,10 +1,25 @@
 # import cv2 library
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawings = mp.solutions.drawing_utils
 
 # Capture the webcam. 0 is the internal camera and 1 ia for an external camera
 webcam = cv2.VideoCapture(0)
 while True:
     ret,frame = webcam.read()
+
+    # Apply the hand tracking model
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = mp_hands.Hands(max_num_hands=3, min_detection_confidence=0.7).process(frame)
+
+    # Draw annotations on the image
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for landmarks in results.multi_hand_landmarks:
+            mp_drawings.draw_landmarks(frame, landmarks, connections=mp_hands.HAND_CONNECTIONS)
+
 
     if ret == True:
         flipped_frame = cv2.flip(frame, 1)
