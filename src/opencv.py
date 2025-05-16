2e653943bf3f9df619a5146efc513252508d2bca
diego-A-castro <daccastro05@gmail.com>
2025-05-15 21:43:15 -0500

    added unfinished finger counter, It is not yet working.

--- src/opencv.py
+++ src/opencv.py
@@ -1,6 +1,18 @@
 # import cv2 library
 import cv2
 import mediapipe as mp
+import numpy as np
+
+def count_fingers(hand_landmarks):
+    finger_tips = [8,12,16,20]
+    fingers_up = 0
+    if hand_landmarks[finger_tips].y > hand_landmarks[finger_tips - 2].y:
+        fingers_up += 1
+
+    if hand_landmarks[4].x < hand_landmarks[1].x:
+        fingers_up += 1
+
+    return fingers_up
 
 mp_hands = mp.solutions.hands
 mp_drawings = mp.solutions.drawing_utils
@@ -18,7 +30,11 @@ while True:
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
         for landmarks in results.multi_hand_landmarks:
+            fingers_up = count_fingers(landmarks)
             mp_drawings.draw_landmarks(frame, landmarks, connections=mp_hands.HAND_CONNECTIONS)
+
+            cv2.putText(np.zeros((500, 500, 3), dtype=np.uint8), 'Fingers: {fingers_up}', (50, 100), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
 
     if ret == True:
